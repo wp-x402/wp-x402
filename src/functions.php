35d1dd7eb43b7d3ebcf9e9735550c1064d2dc85b
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-12-19 20:49:28 -0800

    Add telemetry function helper.

--- src/functions.php
+++ src/functions.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace TheFrosty\WpX402;
 
+use TheFrosty\WpX402\Api\Api;
+use TheFrosty\WpX402\Content\Payment;
 use TheFrosty\WpX402\Middleware\Middleware;
 use TheFrosty\WpX402\Middleware\Rejection;
+use TheFrosty\WpX402\Telemetry\EventType;
 
 /**
  * Returns the Middleware.
@@ -29,4 +32,27 @@ function middleware(): Middleware
 function reject(string $code, string $message, int|null $status): Rejection
 {
     return new Rejection([Rejection::CODE => $code, Rejection::MESSAGE => $message, Rejection::STATUS => $status]);
+}
+
+/**
+ * Telemetry API.
+ * @param EventType $event_type
+ * @param array $meta
+ * @throws \JsonException
+ */
+function telemetry(EventType $event_type, array $meta = []): void
+{
+    $wallet = get_option('x402_wallet', Payment::TESTNET_WALLET);
+    $price = get_option('x402_price', Payment::DEFAULT_PRICE);
+
+    $data = [
+        Api::ACTION => Api::ACTION_COLLECT,
+        'event_type' => $event_type->value,
+        'project_type' => 'wordpress-plugin',
+        'wallet' => $wallet,
+        'amount' => $price,
+        'meta' => $meta,
+    ];
+
+    Api::wpRemote(Api::getApiUrl(), $data);
 }
