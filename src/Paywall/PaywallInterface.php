a312e7d6a7f1f8bdc9f1db7543e95ea2f5433380
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-12-22 09:57:49 -0800

    Remove wallet const value.

--- src/Paywall/PaywallInterface.php
+++ src/Paywall/PaywallInterface.php
@@ -11,5 +11,4 @@ namespace TheFrosty\WpX402\Paywall;
 interface PaywallInterface
 {
     public const float DEFAULT_PRICE = 0.01;
-    final public const string TESTNET_WALLET = '0x505bc35f0a83c9ed06c6f94e68f0f86cf2812a6b'; // phpcs:ignore
 }
