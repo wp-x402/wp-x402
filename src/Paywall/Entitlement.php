152d644c09a5155e88a4601599885f0e67ef18e7
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-12-25 09:42:26 -0800

    Add Entitlement Enum

---
+++ src/Paywall/Entitlement.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheFrosty\WpX402\Paywall;
+
+use function esc_html__;
+
+enum Entitlement: string
+{
+    case DIGITAL = 'digital';
+    case PAYMENT_REQUIRED = 'payment-required';
+
+    /**
+     * Returns array of entitlement choices with labels and values.
+     * @return array
+     */
+    public static function choices(): array
+    {
+        return array_combine(
+            array_map(static fn(Entitlement $entitlement) => $entitlement->value, self::cases()),
+            array_map(static fn(Entitlement $entitlement) => $entitlement->label(), self::cases())
+        );
+    }
+
+    /**
+     * Returns label match for value.
+     * @return string
+     */
+    public function label(): string
+    {
+        return match ($this) {
+            self::DIGITAL => esc_html__('Digital Access', 'wp-x402'),
+            self::PAYMENT_REQUIRED => esc_html__('Payment Required', 'wp-x402'),
+        };
+    }
+}
