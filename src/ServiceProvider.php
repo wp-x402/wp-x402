abe2fafc3d302de5cc6974d08a57d2b997d22172
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-12-31 16:40:41 -0800

    Setup the License Manager settings array

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace TheFrosty\WpX402;
 
+use Dwnload\EddSoftwareLicenseManager\Edd\AbstractLicenceManager;
 use Dwnload\WpSettingsApi\WpSettingsApi;
 use Multicoin\AddressValidator\CurrencyFactory;
 use Multicoin\AddressValidator\WalletAddressValidator;
@@ -11,6 +12,7 @@ use Pimple\Container as PimpleContainer;
 use Pimple\ServiceProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use TheFrosty\WpX402\Settings\Factory;
+use function get_option;
 
 /**
  * Class ServiceProvider
@@ -20,6 +22,7 @@ class ServiceProvider implements ServiceProviderInterface
 {
 
     public const string HTTP_FOUNDATION_REQUEST = 'http.request';
+    public const string LICENSE_DATA = 'license_data';
     public const string WALLET_ADDRESS_VALIDATOR = 'address_validator';
     public const string WP_SETTINGS_API = 'wp_settings_api';
 
@@ -30,6 +33,17 @@ class ServiceProvider implements ServiceProviderInterface
     public function register(PimpleContainer $pimple): void
     {
         $pimple[self::HTTP_FOUNDATION_REQUEST] = static fn(): Request => Request::createFromGlobals();
+        $pimple[self::LICENSE_DATA] = static function (): array {
+            $license = get_option(AbstractLicenceManager::LICENSE_SETTING, []);
+            return [
+                'api_url' => 'https://wp-x402.com/edd-sl-api',
+                'license' => $license[PLUGIN_SLUG]['license'] ?? '',
+                'item_name' => 'x402 for WordPress', // Name of this plugin (matching your EDD Download title).
+                'author' => 'wp-x402',
+                'item_id' => 14,
+                'version' => VERSION,
+            ];
+        };
         $pimple[self::WALLET_ADDRESS_VALIDATOR] = static fn(): WalletAddressValidator => new WalletAddressValidator(
             CurrencyFactory::createRegistry()
         );
