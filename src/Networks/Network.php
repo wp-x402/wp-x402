a882c1f90febae22a388ff497e9d7938ad1f76fa
Austin Passy <367897+thefrosty@users.noreply.github.com>
2026-01-03 17:17:02 -0800

    Update Network interface to get dynamic class/Enum member fetch

--- src/Networks/Network.php
+++ src/Networks/Network.php
@@ -11,4 +11,19 @@ namespace WpX402\WpX402\Networks;
 interface Network
 {
 
+    /**
+     * Get Enum member fetch.
+     * @ref https://php.watch/versions/8.3/dynamic-class-const-enum-member-syntax-support
+     * @param string $asset
+     * @return self
+     */
+    public static function getAsset(string $asset): self;
+
+    /**
+     * Get Enum member fetch.
+     * @ref https://php.watch/versions/8.3/dynamic-class-const-enum-member-syntax-support
+     * @param string $asset
+     * @return self
+     */
+    public static function getBase(string $asset): self;
 }
