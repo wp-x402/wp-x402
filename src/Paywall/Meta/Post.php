abc56d6ee0dea6348b83453b178cdb13f45eb32d
Austin Passy <367897+thefrosty@users.noreply.github.com>
2026-01-16 10:47:53 -0800

    Add Paywall meta for Posts

---
+++ src/Paywall/Meta/Post.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WpX402\WpX402\Paywall\Meta;
+
+use Carbon_Fields\Exception\Incorrect_Syntax_Exception;
+use Carbon_Fields\Field\Field;
+use WpX402\WpX402\Integration\CarbonFields\CarbonFields;
+use WpX402\WpX402\Paywall\PaywallInterface;
+use function esc_html;
+
+/**
+ * Class Post
+ * @package WpX402\WpX402\Paywall\Meta
+ */
+class Post extends CarbonFields
+{
+
+    /**
+     * @throws Incorrect_Syntax_Exception
+     */
+    protected function registerFields(): void
+    {
+        $this->postMetaContainer(esc_html('x402 Paywall Settings'))
+            ->where('post_type', '=', 'post')
+            ->add_fields($this->getFields())
+            ->set_context('side')
+            ->set_priority('low');
+    }
+
+    /**
+     * Get our fields array.
+     * @return Field[]
+     */
+    private function getFields(): array
+    {
+        return [
+            $this->createCheckboxField(PaywallInterface::PAYWALL_ENABLED, esc_html__('Enable Paywall', 'wp-x402')),
+        ];
+    }
+}
